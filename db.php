f48d8725273df079cd22a4319a42f4a5f16d0257
cedcedie <cedricjamesbulan@gmail.com>
2025-04-25 23:19:50 +0800

    dashboard

---
+++ db.php
@@ -0,0 +1,18 @@
+<?php
+
+$host = "localhost"; 
+$port = "5432"; 
+$dbname = "libraryDB"; 
+$user = "postgres"; 
+$password = "librarySystem"; 
+
+try {
+    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
+    $pdo = new PDO($dsn, $user, $password);
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected to PostgreSQL successfully!";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
